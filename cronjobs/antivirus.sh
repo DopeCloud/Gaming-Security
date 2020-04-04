fc56dbe906974405f226b4c0e29ad4d774e28477
ƬHE ЯAW ☣ <daraw665@gmail.com>
2020-04-05 00:41:59 +0200

    Update antivirus.sh

--- cronjobs/antivirus.sh
+++ cronjobs/antivirus.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Scan and remove.
-clamscan -r --bell -i /home/ >> /root/antivirus-logs
+#clamscan -r --bell -i /home/ >> /root/antivirus-logs
 
 # Clear RAM Caches.
 sync; echo 3 > /proc/sys/vm/drop_caches
-swapoff -a && swapon -a
+#swapoff -a && swapon -a
